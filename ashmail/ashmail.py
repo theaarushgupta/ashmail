b1420d991c406e64bc39de1fbdb20aaefda61fa9
Aarush Gupta <aarush@theaarushgupta.com>
2021-04-22 22:13:35 -0700

    release: v0.1.0 - optimized ashmail for larger email lists with threading

--- ashmail/ashmail.py
+++ ashmail/ashmail.py
@@ -2,6 +2,7 @@ from yaml import safe_load
 from csv import reader
 from jinja2 import Template
 from argparse import ArgumentParser
+from threading import Thread
 
 from ashmail.send import Send
 
@@ -30,24 +31,22 @@ class AshMail:
         parsedSubject = template.render(name = name, subject = subject)
         self.configuration["subject"] = parsedSubject
 
-    def run(self) -> None:
-        for name in self.recievers:
-            email = self.recievers[name]
-            content = self.template.render(name = name, email = email)
-            self.parseSubject(
-                self.configuration["subject"],
-                name,
-                email
-            )
-            send = Send(
-                self.configuration["from"],
-                self.configuration["password"],
-                email,
-                self.configuration["subject"],
-                content
-            )
-            send.send()
-            print(f"[INFO] Successfully sent email to {name} ({email})")
+    def run(self, name: str, email: str) -> None:
+        content = self.template.render(name = name, email = email)
+        self.parseSubject(
+            self.configuration["subject"],
+            name,
+            email
+        )
+        send = Send(
+            self.configuration["from"],
+            self.configuration["password"],
+            email,
+            self.configuration["subject"],
+            content
+        )
+        send.send()
+        print(f"[INFO] Successfully sent email to {name} ({email})")
 
 def retrieveConfigurationFile() -> str:
     argumentParser = ArgumentParser(
@@ -67,7 +66,12 @@ def main() -> None:
         content = configuration["content"]
         recievers = configuration["recievers"]
         ashmail = AshMail(configuration, content, recievers)
-        ashmail.run()
+        print("[INFO] Initializing Threads")
+        for name in ashmail.recievers:
+            email = ashmail.recievers[name]
+            email = email.replace(" ", "")
+            thread = Thread(target = ashmail.run, args = (name, email))
+            thread.start()
     except FileNotFoundError:
         print("[ERROR] File Not Found")
         print(" Check the command-line arguments and configuration file for spelling errors.")
--- ashmail/send.py
+++ ashmail/send.py
@@ -1,5 +1,4 @@
 from smtplib import SMTP
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
